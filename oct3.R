3c09ca157eb1ff19d2d14dc575134e7a5793f93d
Lance Bachmeier <bachmeil@yahoo.com>
2023-10-05 15:27:08 -0500

    Update Oct 3 with the Oct 5 code

--- oct3.R
+++ oct3.R
@@ -89,4 +89,91 @@ predict(arma.best, 12)
 # conditioning on the month
 # and downward trend
 
+cyclical.hat <- predict(arma.best, 12)$pred
+cyclical.hat
+plot(cyclical.hat)
+
+# Seasonal forecast
+fit.seasonal
+seas <- fit.seasonal$coefficients
+seas
+seas[8:12]
+seas[2:7]
+c(seas[8:12], 0, seas[2:7])
+# That is month-specific effect
+# Still need the intercept (common effect)
+tmp <- -89.6 + c(seas[8:12], 0, seas[2:7])
+# Set time series properties
+seasonal.hat <- ts(tmp, start=c(2023,8),
+                   frequency=12)
+seasonal.hat
+
+# Linear trend forecast
+fit.trend
+length(autos)
+752.9-0.6675*679
+# 299.6675 is the starting point for our
+# forecasts
+(1:12)*-0.6675
+299.6675 + (1:12)*-0.6675
+trend.hat <- ts(299.6675 + (1:12)*-0.6675,
+                start=c(2023,8),
+                frequency=12)
+trend.hat
+autos.hat <- trend.hat +
+  seasonal.hat + cyclical.hat
+autos.hat
+plot(autos.hat)
+# Let's remove the axis labels and
+# add a title
+plot(autos.hat, xlab="", ylab="")
+plot(autos.hat, xlab="", ylab="",
+     main="US Sales of Domestic Autos, 000s Per Month",
+     lwd=1.4)
+plotdata <- c(window(autos, 
+                     start=c(2020,8)),
+              autos.hat)
+plotdata <- c(window(autos, 
+                     start=c(2020,8)),
+                     autos.hat)
+plotdata <- ts(c(window(autos, 
+                     start=c(2020,8)),
+                     autos.hat),
+               start=c(2020,8),
+               frequency=12)
+plot(plotdata, xlab="", ylab="",
+     main="US Sales of Domestic Autos, 000s Per Month",
+     lwd=1.4)
+abline(v=2023.6, lty=2, col="orange",
+       lwd=2)
+
+# YOY percentage change of autos
+dautos <- pctChange(autos, 12)
+
+# Predict dautos using an ARMA
+arma.best <- armafit(dautos,
+                     ar=6, ma=6,
+                     auto=TRUE)
+arma.best
+dautos.hat <- predict(arma.best,
+                      12)$pred
+dautos.hat
+dautos
+# Get the base with the right
+# time series properties
+base <- window(autos, 
+               start=c(2022,8))
+base <- ts(base, start=c(2023,8),
+           frequency=12)
+autos.hat2 <- base * (1+dautos.hat)
+ts.combine(autos.hat, autos.hat2)
+
+plotdata <- ts(c(window(autos, 
+              start=c(2020,8)),
+              autos.hat2),
+              start=c(2020,8),
+              frequency=12)
+plot(plotdata, xlab="", ylab="",
+     main="US Sales of Domestic Autos, 000s Per Month",
+     lwd=1.4)
 
